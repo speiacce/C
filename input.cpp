b250d436bf7d111daedcc9e5720345490549fd1a
spppppp <1187628413@qq.com>
2022-01-19 19:12:52 +0800

    master

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string first_name,last_name;//那咋办嘛，日日日日
+    cout<<"不要用中文:";
+    cin>>first_name;
+    cout<<"学英语吧:";
+    cin>>last_name;
+    cout<<"日了狗了:"<<first_name+" "+last_name;
+    return 0;
+}
